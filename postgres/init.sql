8b47bff5ddad23d050e0c7d767d4658419178ae0
phazyy <phazyy9@gmail.com>
2016-11-27 19:45:59 +0000

    Initial commit of Postgres Dockerfile

---
+++ postgres/init.sql
@@ -0,0 +1,10 @@
+CREATE USER docker;
+CREATE DATABASE docker;
+GRANT ALL PRIVILEGES ON DATABASE docker TO docker;
+
+CREATE TABLE employees (
+  id          serial PRIMARY KEY,
+  forename    varchar(40),
+  surname     varchar(40),
+  description varchar(120)
+);
