03b77e4a495732f6585002a6f90e734c43591a6c
Adam Houston <phazyy9@gmail.com>
2016-12-02 21:56:27 +0000

    psql: Inserting mock employee data

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,4 +1,3 @@
-CREATE DATABASE apex;
 GRANT ALL PRIVILEGES ON DATABASE docker TO docker;
 
 CREATE TABLE employees (
@@ -7,3 +6,20 @@ CREATE TABLE employees (
   surname     varchar(40),
   description varchar(120)
 );
+
+insert into employees (forename, surname, description) values 
+  ('Aaron',   'Shaw',       'Research Assistant III'),
+  ('Jimmy',   'Henry',      'Marketing Manager'),
+  ('Howard',  'Hansen',     'Data Coordiator'),
+  ('Brenda',  'Montgomery', 'Information Systems Manager'),
+  ('Jack',    'Andrews',    'Internal Auditor'),
+  ('William', 'Scott',      'Administrative Officer'),
+  ('Emily',   'Johnston',   'Data Coordiator'),
+  ('Stephen', 'Webb',       'Senior Financial Analyst'),
+  ('Donna',   'Little',     'Environmental Tech'),
+  ('Tammy',   'Thompson',   'Analyst Programmer'),
+  ('Denise',  'Mitchell',   'Senior Sales Associate'),
+  ('Philip',  'Jordan',     'Internal Auditor'),
+  ('Jean',    'Burke',      'Software Consultant'),
+  ('Dennis',  'Griffin',    'Senior Cost Accountant'),
+  ('Linda',   'Bishop',     'Mechanical Systems Engineer');
