085f608011841796eb8c5351e5fd5e718a053a73
Adam Houston <phazyy9@gmail.com>
2017-05-27 16:57:39 +0100

    [api] : Adding more temp fake data for testing

--- api/src/main/java/io/ahouston/utils/DatabaseLoader.java
+++ api/src/main/java/io/ahouston/utils/DatabaseLoader.java
@@ -22,8 +22,16 @@ public class DatabaseLoader implements CommandLineRunner {
 
     @Override
     public void run(String... strings) throws Exception {
-        this.repository.save(new Employee(
-                "Frodo", "Baggins", "Ring Bearer"
-        ));
+        this.repository.save(new Employee("Frodo",    "Baggins",    "Ring Bearer"));
+        this.repository.save(new Employee("Ewart",    "Prichard",   "Design Manager"));
+        this.repository.save(new Employee("Vincent",  "Dorr",       "Media Manager"));
+        this.repository.save(new Employee("Chico",    "Ball",       "Systems Analyst"));
+        this.repository.save(new Employee("Mikel",    "Fonso",      "Financial Advisor"));
+        this.repository.save(new Employee("Clemente", "Myford",     "Power Engineer"));
+        this.repository.save(new Employee("Delainey", "Dooland",    "Programmer"));
+        this.repository.save(new Employee("Garrett",  "Milham",     "Social Worker"));
+        this.repository.save(new Employee("Rabbi",    "Zoane",      "Community Outreach"));
+        this.repository.save(new Employee("Moe",      "Confort",    "Financial Analyst"));
+        this.repository.save(new Employee("Carson",   "Sharphurst", "Statistician"));
     }
 }
