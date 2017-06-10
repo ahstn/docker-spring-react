20128c844fb4d5203ddfaea54c46553c144cc0a4
Adam Houston <phazyy9@gmail.com>
2017-06-10 18:00:09 +0100

    [api] : adding config to serialize entity id

---
+++ api/src/main/java/io/ahouston/repositories/RepositoryConfig.java
@@ -0,0 +1,14 @@
+package io.ahouston.repositories;
+
+import io.ahouston.entities.Employee;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+
+@Configuration
+public class RepositoryConfig extends RepositoryRestConfigurerAdapter {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(Employee.class);
+    }
+}
