f460ec0574e082141d9144068467fa466d163b3e
Adam Houston <phazyy9@gmail.com>
2018-05-10 05:31:00 +0100

    #1 : Restructuring

--- api/src/test/java/io/ahstn/DockerSpringVue/DockerSpringVueApplicationTests.java
+++ api/src/test/java/io/ahstn/DockerSpringVue/DockerSpringVueApplicationTests.java
@@ -1,4 +1,4 @@
-package io.ahouston;
+package io.ahstn.DockerSpringVue;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ApexApplicationTests {
+public class DockerSpringVueApplicationTests {
 
 	@Test
 	public void contextLoads() {
